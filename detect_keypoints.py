63564914eb19060d4fdd19da42e6e93abc90c957
Toshikazu Ohtsuki <toshikaz@gmail.com>
2020-02-16 21:00:57 +0900

    Add file via upload

---
+++ detect_keypoints.py
@@ -0,0 +1,67 @@
+import os
+import glob
+import argparse
+
+import cv2
+
+def detectKeyPoints(detect_image):
+
+    detector = cv2.AKAZE_create()
+    keypoints = detector.detect(detect_image)
+
+    return keypoints
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='detect keypoints from imagefile and do data argumentation.')
+    parser.add_argument('-p', required=True, help='set files path.', metavar='imagefile_path')
+    args = parser.parse_args()
+
+    detect_dir = args.p + "/_detectedKeypoints"
+    if not os.path.exists(detect_dir):
+        os.makedirs(detect_dir)
+
+    # pngファイル取得
+    files = glob.glob("%s/*.png" % (args.p))
+
+    for file_name in files:
+        before_path = file_name
+        filename = os.path.basename(file_name)
+        after_path = '%s/%s' % ( detect_dir, filename )
+
+        print("filename = "+filename)
+
+    detected_files = glob.glob(detect_dir+"/*.png")
+    
+    # 画像のロードcheck
+    #image = cv2.imread(file_name)
+    #print(image)
+
+    for detected_file_name in detected_files:
+        print(" detect key points on file:"+detected_file_name)
+
+        # 画像のロード
+        image = cv2.imread(detected_file_name)
+        print(image)
+
+        if image is None:
+            # 読み込み失敗
+            print("image is None")
+            continue
+
+
+        detectKeyPoints_list = detectKeyPoints(image)
+        #if len(detectKeyPoints_list) == 0:
+        #    continue
+
+        basename = os.path.basename(detected_file_name)
+
+        out_image = cv2.drawKeypoints(image, detectKeyPoints_list, None)
+        
+        # 出力
+        cv2.imwrite(detect_dir+"/"+"KeyPoints_"+basename+".png", out_image)
+
+        print("loop end")
+
+
+
+    print("Done.")
